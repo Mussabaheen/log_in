92e1486e304fda875deb07cccf77f70b2dbc4ba9
mussabaheen@gmail.com <mussabaheen@gmail.com>
2019-04-25 18:57:56 +0500

    Log in Page

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,21 @@ class _loginpageState extends State<loginpage> with SingleTickerProviderStateMix
     Animation<double> iconanimation;
 
 
-    @override
+    @override  //implementing animations here
     void initState() {
           super.initState();
+          _iconanimationcontroller=new AnimationController(
+            vsync: this,
+            duration:new Duration(
+              microseconds: 300
+            )
 
+          );
+          iconanimation=new CurvedAnimation(parent: _iconanimationcontroller, curve: Curves.bounceInOut);
+
+          iconanimation.addListener(()=>this.setState((){}));
+          _iconanimationcontroller.forward();
+          
 
     }
 
@@ -42,6 +53,7 @@ class _loginpageState extends State<loginpage> with SingleTickerProviderStateMix
     return new Scaffold(
       backgroundColor: Colors.greenAccent,
       body: new Stack(
+
           fit: StackFit.expand,
           children: <Widget>[
             new Image(
@@ -56,13 +68,59 @@ class _loginpageState extends State<loginpage> with SingleTickerProviderStateMix
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   new FlutterLogo(
-                    size: 130,
+                    size: _iconanimationcontroller.value*100,
 
 
+                  ),
+                  new Form(
+                      child:new Theme(
+                        data: new ThemeData(
+                            brightness:Brightness.dark,
+                            primarySwatch: Colors.teal,
+                            inputDecorationTheme:new InputDecorationTheme(labelStyle: new TextStyle(color: Colors.teal,fontSize: 20))
+                        ),
+                        child: Container(
+                          padding: const EdgeInsets.all(40.0),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+
+                            children: <Widget>[
+                              new TextFormField(
+                                decoration: new InputDecoration(
+                                    labelText: "Enter Email",
+
+
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              new TextFormField(
+                                decoration: new InputDecoration(
+                                  labelText: "Enter Password",
+
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                                obscureText: true,
+                              ),
+                              new Padding(padding: const EdgeInsets.all(20.0)),
+                              new RaisedButton(
+                                textColor: Colors.white,
+                                color: Colors.teal,
+                                child: new Text("Log in"
+                                ),
+                                onPressed: ()=>{},
+
+                              )
+                            ],
+                          ),
+                        ),
+                      )
                   )
                 ],
 
-            )
+            ),
+
+
+
           ],
       ),
     );
